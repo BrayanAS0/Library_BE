97cb03101cbd7cc37f327578449dd8464818658e
BrayanAS0 <el.el.brayan16@gmail.com>
2025-07-18 22:42:59 -0600

    new urls

--- minilibrary/urls.py
+++ minilibrary/urls.py
@@ -1,1 +1,6 @@
 from django.http import JsonResponse,HttpResponse
+from django.urls import path
+from . import views
+urlpatterns = [
+    path("get_books/",views.get_books)
+]
--- minilibrary/views.py
+++ minilibrary/views.py
@@ -1,3 +1,16 @@
+from django.http import JsonResponse
+from .models import Book
 from django.shortcuts import render
 
-# Create your views here.
+from django.http import JsonResponse
+from .models import Book
+def get_books(request):
+    author_id = request.GET.get("author")
+    books = Book.objects.all()
+
+    if author_id:
+        books = books.filter(author__pk=author_id)
+
+    books_data = books.values()  # convierte queryset a lista de diccionarios
+    return render(request,"minilibrary/minilibrary.html",{"author":"brayan","books":books_data})
+    # return JsonResponse(list(books_data), safe=False)
