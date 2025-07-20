380bb8648c053da515940dabf4e2610a39625b22
BrayanAS0 <el.el.brayan16@gmail.com>
2025-07-19 21:49:55 -0600

    added cors

--- minilibrary/views.py
+++ minilibrary/views.py
@@ -1,9 +1,6 @@
 from django.http import JsonResponse
 from .models import Book
-from django.shortcuts import render
 
-from django.http import JsonResponse
-from .models import Book
 def get_books(request):
     author_id = request.GET.get("author")
     books = Book.objects.all()
@@ -11,6 +8,9 @@ def get_books(request):
     if author_id:
         books = books.filter(author__pk=author_id)
 
-    books_data = books.values()  # convierte queryset a lista de diccionarios
+    books_data = list(books.values())  # <-- con list()
+
+    return JsonResponse(books_data, safe=False)
+
     return render(request,"minilibrary/minilibrary.html",{"author":"brayan","books":books_data})
     # return JsonResponse(list(books_data), safe=False)
--- playground/settings.py
+++ playground/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders'
 ]
 
 MIDDLEWARE = [
@@ -50,6 +51,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware'
 ]
 
 ROOT_URLCONF = 'playground.urls'
@@ -123,7 +125,7 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     BASE_DIR / "static"
 ]
-
+CORS_ALLOWED_ORIGINS= ["http://localhost:5173"]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
